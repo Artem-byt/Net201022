dbe6abea381b6afed1bb0329cd7151ac0eb544d5
Artem-byt <artem191020001@gmail.com>
2023-09-08 22:25:36 +0300

    added Buttons

--- Assets/Scripts/Lesson_4/Lesson_6/LobbyUIWindow.cs
+++ Assets/Scripts/Lesson_4/Lesson_6/LobbyUIWindow.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 public class LobbyUIWindow : MonoBehaviour
 {
     public Button CloseRoom;
+    public Button OpenRoom;
     public Button Connect;
     public Button CloseLobby;
     public Button PrivateRoom;
+    public Button PublicRoom;
     public GameObject LobbyWindow;
     public UICatalogItem _lobbyCatalogItem;
 
